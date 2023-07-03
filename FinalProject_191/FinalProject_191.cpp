c61db0c1f5d97af8acab17f8f4a1cabb060514d9
tedyistiawan <tedypnd07@gmail.com>
2023-07-03 14:49:10 +0700

    1

--- FinalProject_191/FinalProject_191.cpp
+++ FinalProject_191/FinalProject_191.cpp
@@ -11,15 +11,72 @@ public:
 	virtual void input() {}
 	virtual float Luas(int a) { return 0; }
 	virtual float Keliling(int a) { return 0; }
-	virtual void cekUkuran() { return 0; }
+	virtual void cekUkuran() { return ; }
 	void setX(int a) {
 		this->x = a;
 	}
 	int getX() {
 		return x;
 	}
-	/*lengkapi disini*/
+
 };
-class Lingkaran :public bidangDatar { /*lengkapi disini*/ };
-class Persegipanjang :public bidangDatar { /*lengkapi disini*/ };
-int main() { /*lengkapi disini*/ }
+class Lingkaran :public bidangDatar {
+public:
+	int r;
+
+	Lingkaran() :
+		bidangDatar() {
+		cout << "Lingkaran dibuat" << endl;
+	}
+
+	void input() {
+		cout << "Masukkan jejari : ";
+		cin >> r;
+		setX(r);
+	}
+
+	double Luas() {
+		int r = getX();
+		return 6 * r * r;
+	}
+
+	double Keliling() {
+		int r = getX();
+		return 37.68 * 6 * r;
+	}
+};
+
+class Persegipanjang :public bidangDatar {
+public:
+	int s;
+
+	Persegipanjang(){
+		cout << "\nPersegiPanjang dibuat" << endl;
+	}
+	void input() {
+		cout << "Masukkan sisi : ";
+		cin >> s;
+		setX(s);
+	}
+	float Luas() {
+		int s = getX();
+		return 30 * s;
+	}
+	float Keliling() {
+		int s = getX();
+		return 22 * s;
+	}
+
+};
+
+int main() {
+	Lingkaran L;
+	L.input();
+	cout << "Luas Lingkaran = " << L.Luas() << "\nKeliling Lingkaran = " << L.Keliling() << endl;
+
+	Lingkaran B;
+	B.input();
+	cout << "Luas BidangDatar = " << B.Luas() << "\nKeliling Lingkaran = " << B.Keliling() << endl;
+
+	return 0;
+}
